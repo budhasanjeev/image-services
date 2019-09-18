af47198016ebf42b564154b1bf6bfa65a06cf3c4
budha.sanjeev <budha.sanjeev@showcase-tv.com>
2019-09-18 16:51:34 +0900

    line authorization url

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,6 @@
 
 console.log('client side code running.... ');
-var webclient = require("request");
+
 
 const lineButton = document.getElementById('btn-line');
 const facebookButton = document.getElementById('btn-facebook');
@@ -9,20 +9,6 @@ const wechatButton = document.getElementById('btn-wechat');
 
 lineButton.addEventListener('click', function(e) {
   console.log('LINE .....');
-
-  webclient.get({
-    url: 'https://access.line.me/oauth2/v2.1/authorize',
-    qs: {
-      response_type: 'string',
-      client_id: '1622665154',
-      redirect_uri: 'https://secure-beach-84908.herokuapp.com/',
-      state: '1234',
-      scope: 'profile'
-    }
-  }, function (error, response, body) {
-    console.log(body);
-  });
-
 });
 
 facebookButton.addEventListener('click', function(e) {
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
+var webclient = require("request");
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-var webclient = require("request");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,9 +12,18 @@ app.get('/', function(req, res) {
 
 app.get('/auth', function(req, res) {
 
-  var appName = req.query.appName;
-
-
+  webclient.get({
+    url: 'https://access.line.me/oauth2/v2.1/authorize',
+    qs: {
+      response_type: 'string',
+      client_id: '1622665154',
+      redirect_uri: 'https://secure-beach-84908.herokuapp.com/',
+      state: '1234',
+      scope: 'profile'
+    }
+  }, function (error, response, body) {
+    console.log(body);
+  });
 });
 
 
