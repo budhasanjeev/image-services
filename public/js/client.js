bcc1b3d84b41f947710dae0e30dbc9b5f7da3ff7
budha.sanjeev <budha.sanjeev@showcase-tv.com>
2019-09-20 12:29:05 +0900

    facebook auth

--- public/js/client.js
+++ public/js/client.js
@@ -4,42 +4,13 @@ console.log('client side code running.... ');
 $('#btn-line').click(function(e) {
   console.log('LINE .....');
 
-  $('.loading').removeClass('hide');
-
-  $.ajax({
-    url: 'auth/line',
-    type: 'post',
-    success: function(res) {
-      console.log('res = '+ res);
-    },
-    error: function(err) {
-      console.log('err = '+ JSON.stringify(err));
-    },
-    complete: function() {
-      $('.loading').addClass('hide');
-    }
-  });
-
+  ajaxCall('/auth/line', 'post');
 });
 
 $('#btn-facebook').click(function(e) {
   console.log('facebook .....');
 
-  $('.loading').removeClass('hide');
-
-  $.ajax({
-    url: 'auth/facebook',
-    type: 'post',
-    success: function(res) {
-      console.log('res = '+ res);
-    },
-    error: function(err) {
-      console.log('err = '+ JSON.stringify(err));
-    },
-    complete: function() {
-      $('.loading').addClass('hide');
-    }
-  });
+  // ajaxCall('/auth/facebook', 'post');
 
 });
 
@@ -50,5 +21,26 @@ $('btn-instagram').click(function(e) {
 $('#btn-wechat').click(function(e) {
   console.log('wechat .....');
 });
+
+
+function ajaxCall(urlString, methodType) {
+
+  $('.loading').removeClass('hide');
+
+  $.ajax({
+    url: urlString,
+    type: methodType,
+    success: function(res) {
+      console.log('res = '+ res);
+    },
+    error: function(err) {
+      console.log('err = '+ JSON.stringify(err));
+    },
+    complete: function() {
+      $('.loading').addClass('hide');
+    }
+  });
+
+}
 
 
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const LineStrategy =  require('passport-line2');
-const FacebookStrategy = require('passport-facebook');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
@@ -12,14 +11,7 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname+ '/views/index.html');
 });
 
-// serialize and deserialize user
-passport.serializeUser((user, done) => {
-  done(null, user)
-})
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj)
-})
+app.use(passport.initialize());
 
 passport.use(new LineStrategy(
   {
@@ -33,45 +25,23 @@ passport.use(new LineStrategy(
   }
 ));
 
-passport.use(new FacebookStrategy({
-  clientID: '1379715578872813',
-  clientSecret: '4893159dc84c367bd73bf1144b66d32f',
-  callbackURL: "https://oauth-services-app.herokuapp.com/auth/facebook/callback"
-},
-function(accessToken, refreshToken, profile, cb) {
-  User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-    return cb(err, user);
-  });
-}
-));
+// serialize user 
+passport.serializeUser(function(user, done) {
+  done(null, user)
+});
 
-app.use(passport.initialize());
+// deserialize user
+passport.deserializeUser(function(obj, done) {
+  done(null, obj)
+});
 
 app.get('/auth/line/callback', passport.authenticate('line', { failureRedirect: '/login', successRedirect : '/' }), function(req, res) {
     res.redirect('/');
   }
 );
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login', successRedirect : '/' }), function(req, res) {
-  res.redirect('/');
-}
-);
-
 app.post('/auth/line', passport.authenticate('line'));
-
-app.post('/auth/facebook', passport.authenticate('facebook'));
 
 app.listen(port, function() {
   console.log(`The app is listening at localhost:${port}`);
 });
-
-
-
-/*
-1. https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1622665154&redirect_uri=https://oauth-services-app.herokuapp.com/&state=sanjeev1234&scope=openid%20profile&nonce=09876xyz
-
-
-  response from server https://oauth-services-app.herokuapp.com/?code=4HYeXO4ZrkgdN3kqTyh2&state=sanjeev1234
-
-2. https://api.line.me/oauth2/v2.1/4HYeXO4ZrkgdN3kqTyh2
- */
