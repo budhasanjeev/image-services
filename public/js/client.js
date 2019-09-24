10bfdcc7b06b609eb6b6f73d3a0eb010abc405b8
budha.sanjeev <budha.sanjeev@showcase-tv.com>
2019-09-24 17:23:02 +0900

    firebase authentication

--- public/js/client.js
+++ public/js/client.js
@@ -4,7 +4,25 @@ console.log('client side code running.... ');
 $('#btn-line').click(function(e) {
   console.log('LINE .....');
 
-  ajaxCall('/auth/line', 'post');
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  
 });
 
 $('#btn-facebook').click(function(e) {
