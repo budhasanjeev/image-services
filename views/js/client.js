77b7f9f239d2ee818cf901721787c48f04e2deef
Sanjeev Budha <budhasanjeev@gmail.com>
2020-04-08 14:30:17 +0900

    hide a mobile browser's address bar

---
+++ views/js/client.js
@@ -0,0 +1,8 @@
+$(document.body).on('click', '[action-convert]', function() {
+  alert("Convert");
+});
+
+$(document.body).on('click', '[action-download]', function() {
+  alert("Download");
+});
+
